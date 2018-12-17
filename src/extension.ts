0b8cca2618730385517914e37b043032b2eb20bb
Max <mushishi78@gmail.com>
2018-12-17 22:54:51 +0000

    Improve performance
    Debounce with 500 ms window
    
    Only set errors for the current file

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,26 @@
-import * as vscode from 'vscode';
-import { spawn } from 'child_process';
+import * as vscode from 'vscode'
+import * as lodash from 'lodash'
+import { spawn } from 'child_process'
 import { join } from 'path'
 
 export function activate(context: vscode.ExtensionContext) {
-    const diagnosticCollection = vscode.languages.createDiagnosticCollection();
+    const debouncedLint = lodash.debounce(lint, 500)
+    const diagnosticCollection = vscode.languages.createDiagnosticCollection()
     vscode.workspace.onDidChangeTextDocument(e => {
-        if (e.document.languageId === 'css') lint()
+        if (e.document.languageId === 'css') debouncedLint(e.document.fileName)
     })
-    lint()
+    debouncedLint()
 
-    function lint() {
+    function lint(filename?: string) {
         const ps = spawn('seass', [], { cwd: vscode.workspace.rootPath })
         ps.stderr.on('data', (data) => {
-            const entries: [vscode.Uri, vscode.Diagnostic[]][] = []
+            let entries: [vscode.Uri, vscode.Diagnostic[]][] = []
             const lines = data.toString()
             const re = /(.+)\.css:(\d+):(\d+)-(\d+):(\d+) - (.+)/gi
             while (true) {
                 const match = re.exec(lines)
                 if (!match) break
-                const [_, path, lineStart, colStart, lineEnd, colEnd, message] = match
+                const [path, lineStart, colStart, lineEnd, colEnd, message] = match.slice(1)
                 
                 entries.push([
                     vscode.Uri.file(join(vscode.workspace.rootPath || '',`${path}.css`)),
@@ -39,7 +41,24 @@ export function activate(context: vscode.ExtensionContext) {
                     ]
                 ])
             }
-            diagnosticCollection.set(entries)
+
+            // If inital call, set all of the entries
+            if (filename == null) {
+                diagnosticCollection.set(entries)
+                return
+            }
+
+            // Otherwise only set the entry for the current file
+            const uri = vscode.Uri.file(filename)
+            entries = entries.filter(([fullPath, _]) => fullPath.path === uri.path)
+            const groupedEntries = lodash.flatten(entries.map(([_, entry]) => entry))
+            diagnosticCollection.set(uri, groupedEntries)
         })
+
+        // Log other events
+        const log = (prefix: string) => (data: any) => console.log(prefix, data)
+        ps.on('error', log('seass-error:'))
+        ps.on('exit', log('seass-exit:'))
+        ps.stdout.on('data', log('seass-data:'))
     }
 }
